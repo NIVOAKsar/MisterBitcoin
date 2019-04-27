e4ad5bb4d744a685a7fa2fb135985a61e5aa18e0
Niv Saar <nivordsar@gmail.com>
2019-04-28 00:33:50 +0300

    Updates

---
+++ precache-manifest.e0cd5cd96c9e1acb1eb31ed686ff4180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8dd21120993008da14828dc66c2015b",
+    "url": "/MisterBitcoin/static/media/black-theme.b8dd2112.jpg"
+  },
+  {
+    "revision": "e0cd4f8884f106432ea83dee9a52651c",
+    "url": "/MisterBitcoin/static/media/bitcoin.e0cd4f88.png"
+  },
+  {
+    "revision": "ae84232870f761506707",
+    "url": "/MisterBitcoin/static/js/runtime~main.5e0ee549.js"
+  },
+  {
+    "revision": "2a9ce2aa9530bdfdf9c8",
+    "url": "/MisterBitcoin/static/js/main.be1981b1.chunk.js"
+  },
+  {
+    "revision": "13dbe41e1161af16a455",
+    "url": "/MisterBitcoin/static/js/2.536ddb06.chunk.js"
+  },
+  {
+    "revision": "2a9ce2aa9530bdfdf9c8",
+    "url": "/MisterBitcoin/static/css/main.ede1449e.chunk.css"
+  },
+  {
+    "revision": "3c3aeab3c55463748705c97014ddc1bf",
+    "url": "/MisterBitcoin/index.html"
+  }
+];
